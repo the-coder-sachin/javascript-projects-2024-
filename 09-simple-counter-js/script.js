2344d767596b800b6ad7e95035d7b240d197c854
krssachinn@gmail.com <krssachinn@gmail.com>
2024-10-18 19:01:09 +0530

    working good

--- 09-simple-counter-js/script.js
+++ 09-simple-counter-js/script.js
@@ -1,1 +1,33 @@
-console.log('good luck');
+let counter = document.querySelector('.count')
+let increase = document.querySelector('#increase')
+let reset = document.querySelector('#reset')
+let decrease = document.querySelector('#decrease')
+let count = 0;
+
+function increament(value){
+    return value+1;
+}
+
+function decreament(value){
+    return value-1;
+};
+
+function resetfunc(){
+    return 0
+}
+
+increase.addEventListener('click', e=>{
+    count = increament(count);
+    counter.textContent = count;
+})
+
+decrease.addEventListener('click', e=>{
+     count = decreament(count);
+     counter.textContent = count;
+})
+
+reset.addEventListener('click', e=>{
+     count = resetfunc(count);
+     counter.textContent = count;
+})
+
