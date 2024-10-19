27ed8428fd481cbda898b9831378675160835c14
krssachinn@gmail.com <krssachinn@gmail.com>
2024-10-19 13:18:33 +0530

    add custom variable for increament/decreament

--- 09-simple-counter-js/script.js
+++ 09-simple-counter-js/script.js
@@ -2,14 +2,15 @@ let counter = document.querySelector('.count')
 let increase = document.querySelector('#increase')
 let reset = document.querySelector('#reset')
 let decrease = document.querySelector('#decrease')
+let custom = document.querySelector('#custom-add')
 let count = 0;
 
-function increament(value){
-    return value+1;
+function increament(value, custom = 1){
+   return eval(Number(value)+Number(custom));
 }
 
-function decreament(value){
-    return value-1;
+function decreament(value, custom = 1){
+   return eval(Number(value) - Number(custom));
 };
 
 function resetfunc(){
@@ -17,13 +18,24 @@ function resetfunc(){
 }
 
 increase.addEventListener('click', e=>{
-    count = increament(count);
-    counter.textContent = count;
+    if(custom.value){
+        count = increament(count, custom.value)
+        counter.textContent = count;
+    }else{
+        count = increament(count);
+        counter.textContent = count;
+    }
+
 })
 
 decrease.addEventListener('click', e=>{
-     count = decreament(count);
-     counter.textContent = count;
+     if (custom.value) {
+       count = decreament(count, custom.value);
+       counter.textContent = count;
+     } else {
+       count = decreament(count);
+       counter.textContent = count;
+     }
 })
 
 reset.addEventListener('click', e=>{
